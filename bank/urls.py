706cb0e6d75ac09d29a2627ab7506565220b2f87
s.abdugapar <s.abdugapar@almau.edu.kz>
2024-07-19 03:23:58 +0500

    add transfer url

--- bank/urls.py
+++ bank/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('cards/<str:hashed_id>', UserCardsView.as_view(), name='user-cards'),
     path('balances/<str:hashed_id>', UserBalancesView.as_view(), name='user-balances'),
     path('transactions/<str:hashed_id>', UserTransactionsView.as_view(), name='user-transactions'),
+    path('transfer/', transfer, name='transfer'),
 ]
